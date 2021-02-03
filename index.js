69087e028dafc30606138bda1dd4ec7943a1e99b
giegeralexandra <giegeralexandra@gmail.com>
2021-02-03 18:19:52 -0500

    commit tree acfcb6e17363b4b2b3d0863f30fb1b11e12fb771

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+//const { util } = require("chai");
+
 // Write your code here!
+let main = document.getElementById('main')
+main.remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+
+document.head.appendChild(newHeader)
+newHeader.innerHTML = "YOUR-NAME is the champion"
